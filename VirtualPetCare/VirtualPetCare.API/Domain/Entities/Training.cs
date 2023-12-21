536e9e53d44149d33dc4689fb38be1c2143fb154
Burak Şenses <buraksenseees@gmail.com>
2023-12-21 16:02:48 +0300

    Training model created

---
+++ VirtualPetCare/VirtualPetCare.API/Domain/Entities/Training.cs
@@ -0,0 +1,16 @@
+﻿namespace VirtualPetCare.API.Domain.Entities;
+
+public class Training
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Duration { get; set; }
+
+    // Foreign key property
+    public Guid PetId { get; set; }
+
+    // Navigation property
+    public Pet Pet { get; set; }
+}
