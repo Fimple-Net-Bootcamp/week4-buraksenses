58df71646a397e2bdda17c117e1ce6c07231d47b
Burak Şenses <buraksenseees@gmail.com>
2023-12-22 21:48:09 +0300

    User validator

---
+++ VirtualPetCare/VirtualPetCare.API/Application/Validators/User/CreateUserValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using VirtualPetCare.API.Application.DTOs.User;
+
+namespace VirtualPetCare.API.Application.Validators.User;
+
+public class CreateUserValidator : AbstractValidator<CreateUserRequestDto>
+{
+    public CreateUserValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(30).WithMessage("Name has to be a maximum of 30 characters!");
+
+        RuleFor(x => x.Surname)
+            .NotEmpty().WithMessage("Surname is required.")
+            .MaximumLength(30).WithMessage("Surname has to be a maximum of 30 characters!");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .EmailAddress().WithMessage("Invalid email address format.");
+    }
+}
