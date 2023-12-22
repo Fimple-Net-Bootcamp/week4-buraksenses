b1ca82966cfe0248684c39c5028f6a5eda4b8654
Burak Şenses <buraksenseees@gmail.com>
2023-12-22 21:45:30 +0300

    Training validator

--- VirtualPetCare/VirtualPetCare.API/Application/DTOs/Training/CreateTrainingForPetRequestDto.cs
+++ VirtualPetCare/VirtualPetCare.API/Application/DTOs/Training/CreateTrainingForPetRequestDto.cs
@@ -3,8 +3,6 @@
 public class CreateTrainingForPetRequestDto
 {
     public string Name { get; set; }
-
     public int Duration { get; set; }
-
     public Guid PetId { get; set; }
 }
---
+++ VirtualPetCare/VirtualPetCare.API/Application/Validators/Training/CreateTrainingValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using VirtualPetCare.API.Application.DTOs.Training;
+
+namespace VirtualPetCare.API.Application.Validators.Training;
+
+public class CreateTrainingValidator : AbstractValidator<CreateTrainingForPetRequestDto>
+{
+    public CreateTrainingValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .MaximumLength(50).WithMessage("Name must be less than 50 characters.")
+            .Matches("^[^çÇıİğĞöÖşŞüÜ]*$").WithMessage("Name must not contain Turkish characters.");
+
+        RuleFor(x => x.Duration)
+            .NotEmpty().WithMessage("Duration is required.")
+            .GreaterThan(0).WithMessage("Duration must be greater than zero.");
+
+        RuleFor(x => x.PetId)
+            .NotEmpty().WithMessage("PetId is required.")
+            .Must(id => id != Guid.Empty && id.ToString().Length == 36).WithMessage("PetId must be a valid 36-character Guid.");
+    }
+}
